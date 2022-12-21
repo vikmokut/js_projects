daa6960aba635305138eaf6c1eb9247eb277413b
Victor Mokut <vikmokut@gmail.com>
2022-12-21 09:40:38 +0100

    new files

---
+++ js_DOM/main.js
@@ -0,0 +1,6 @@
+// let name = prompt("Enter your name");
+document.getElementById('demo1').innerHTML = 'This is my New DOM practice<br/>I am making progress';
+
+document.getElementById("demo").innerHTML = "Welcome to my World";
+
+document.title
